22febdcdfb262a8ffbb4083edd41b220a9730780
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-04-22 20:45:31 -0400

    Completed Logo and Name

--- src/screens/welcome-screen/index.tsx
+++ src/screens/welcome-screen/index.tsx
@@ -1,10 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
 import SafeAreaWrapper from "components/shared/safe-area-wrapper";
 import { Box, Text } from "components/utils/thems";
+import { colors } from "components/utils/thems/color";
 import { LinearGradient } from "expo-linear-gradient";
 import { AuthScreenNavigationType } from "navigation/types";
-import { Button } from "react-native";
-
+import { Button, Image } from "react-native";
+const ACTIVITY_LOGO =
+  "https://fastly.picsum.photos/id/565/200/300.jpg?hmac=Ho0T-TCTMRX_uDDGzaLhGzTmukSZdDjpGZJTbL0NY3k";
 const WelcomeScreen = () => {
   const navigation = useNavigation<AuthScreenNavigationType<"Welcome">>();
   const navigationToSingInScreen = () => {
@@ -20,15 +22,32 @@ const WelcomeScreen = () => {
         colors={[
           "#ffffff",
           "#eeeeee",
-          "#cccccc",
-          "#aaaaaa",
-          "#eeeeee",
+          "#EFF6FF",
+          "#D6E8FF",
+          "#D6E8FF",
+          "#EFF6FF",
           "#ffffff",
+          "#eeeeee",
         ]}
         style={{ flex: 1 }}
       >
-        <Box>
-          <Text>Welcome Screen</Text>
+        <Box flex={1} justifyContent="center">
+          <Box alignItems="center">
+            <Image
+              source={{
+                uri: ACTIVITY_LOGO,
+                width: 120,
+                height: 120,
+              }}
+            />
+          </Box>
+          <Text
+            textAlign="center"
+            style={{ marginTop: 15, color: colors.emerald400 }}
+            fontWeight="500"
+          >
+            ACTIVITY APPLICATION
+          </Text>
           <Button
             title="Navigate to Sign In"
             onPress={navigationToSingInScreen}
